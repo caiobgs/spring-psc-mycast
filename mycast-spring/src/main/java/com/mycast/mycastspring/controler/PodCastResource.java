9e2f945d7669d3693a4d12cd215bafc3ed09c053
caiobgs <caiobgs2@gmail.com>
2019-06-07 09:05:46 -0300

    add cors anotation

--- mycast-spring/src/main/java/com/mycast/mycastspring/controler/PodCastResource.java
+++ mycast-spring/src/main/java/com/mycast/mycastspring/controler/PodCastResource.java
@@ -3,6 +3,7 @@ package com.mycast.mycastspring.controler;
 import com.mycast.mycastspring.PodCastRepository;
 import com.mycast.mycastspring.model.PodCast;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController    
 @RequestMapping(path="/podcast" ) 
 public class PodCastResource {
